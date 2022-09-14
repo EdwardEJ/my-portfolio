479f191a868acfc239332c28ca1266184d56b505
A <jesus.e.estrada@hotmail.com>
2022-09-14 03:01:48 -0400

    WIP: mobile first header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import menu from '../images/menu.svg';
 
 export default function Navbar() {
 	return (
-		<>
-			<h2>First Name Last Name</h2>
-		</>
+		<header className='header'>
+			<nav className='navbar'>
+				<div className='navbar-container'>
+					<h2>First Name Last Name</h2>
+					<button>
+						<img className='navbar-menu-icon' src={menu} />
+					</button>
+				</div>
+				<div className='navbar-menu-items'>
+					<a href='#About'>About</a>
+					<a href='#Projects'>Projects</a>
+					<a href='#Skills'>Skills</a>
+					<a href='#Contact'>Contact</a>
+				</div>
+			</nav>
+		</header>
 	);
 }
