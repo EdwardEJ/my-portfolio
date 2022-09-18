39e1331225fe24a4ba0010b81ac0b125a825a51e
A <jesus.e.estrada@hotmail.com>
2022-09-18 01:27:33 -0400

    created mobile navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import menu from '../images/menu.svg';
 
 export default function Navbar() {
+	const [btnClassNames, setbtnClassNames] = useState(true);
+
+	const toggleNavItems = () => {
+		setbtnClassNames(!btnClassNames);
+	};
+
 	return (
 		<header className='header'>
 			<nav className='navbar'>
 				<div className='navbar-container'>
 					<h2>First Name Last Name</h2>
-					<button>
+					<button onClick={toggleNavItems} className='navbar-mobile-button'>
 						<img className='navbar-menu-icon' src={menu} />
 					</button>
 				</div>
-				<div className='navbar-menu-items'>
+				<div
+					className={`navbar-menu-items ${
+						btnClassNames ? 'navbar-closed' : ''
+					}`}
+				>
 					<a href='#About'>About</a>
 					<a href='#Projects'>Projects</a>
 					<a href='#Skills'>Skills</a>
