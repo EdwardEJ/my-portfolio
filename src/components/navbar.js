28f5686642858822d20dfa74b00afb4bf9be8381
A <jesus.e.estrada@hotmail.com>
2022-09-18 17:53:34 -0400

    refactored html dropdown and css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,26 +4,24 @@ import menu from '../images/menu.svg';
 export default function Navbar() {
 	const [btnClassNames, setbtnClassNames] = useState(true);
 
-	const toggleNavItems = () => {
+	function toggleNavItems() {
 		setbtnClassNames(!btnClassNames);
-	};
+	}
 
 	return (
 		<header className='header'>
-			<nav className='navbar'>
-				<div className='navbar-container'>
-					<h2>First Name Last Name</h2>
-					<button onClick={toggleNavItems} className='navbar-mobile-button'>
-						<img className='navbar-menu-icon' src={menu} />
-					</button>
-				</div>
+			<h2>First Name Last Name</h2>
+			<div className='navbar-container'>
+				<button onClick={toggleNavItems} className='navbar-mobile-button'>
+					<img className='navbar-menu-icon' src={menu} />
+				</button>
 				<div className={`navbar-menu-items ${btnClassNames ? 'closed' : ''}`}>
 					<a href='#About'>About</a>
 					<a href='#Projects'>Projects</a>
 					<a href='#Skills'>Skills</a>
 					<a href='#Contact'>Contact</a>
 				</div>
-			</nav>
+			</div>
 		</header>
 	);
 }
